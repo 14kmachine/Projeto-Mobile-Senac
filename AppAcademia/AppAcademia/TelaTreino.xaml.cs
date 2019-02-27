86e21b13f82c59a5b92658e309471f6b30996310
gabriel.vteodoro <gabriel.vteodoro@BOT0626768W10-1>
2019-02-27 14:11:07 -0300

    ajsuste nas telas

--- AppAcademia/AppAcademia/TelaTreino.xaml.cs
+++ AppAcademia/AppAcademia/TelaTreino.xaml.cs
@@ -21,5 +21,85 @@ namespace AppAcademia
         {
 
         }
+
+        private void ViewCell_Tapped_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ViewCell_Tapped_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ViewCell_Tapped_3(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ViewCell_Tapped_4(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ViewCell_Tapped_5(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ViewCell_Tapped_6(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ViewCell_Tapped_7(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ViewCell_Tapped_8(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ViewCell_Tapped_9(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ViewCell_Tapped_10(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ViewCell_Tapped_11(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ViewCell_Tapped_12(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ViewCell_Tapped_13(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ViewCell_Tapped_14(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ViewCell_Tapped_15(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ViewCell_Tapped_16(object sender, EventArgs e)
+        {
+
+        }
     }
 }
